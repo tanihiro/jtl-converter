6ec17781f91a8e4395b68e173bd8c631ad9976f5
itani-hiroki <itanihiroki@gmail.com>
2014-01-31 15:59:40 +0900

    first commit

---
+++ lib/jtl/converter.rb
@@ -0,0 +1,7 @@
+require "jtl/converter/version"
+
+module Jtl
+  module Converter
+    # Your code goes here...
+  end
+end
---
+++ lib/jtl/converter/version.rb
@@ -0,0 +1,5 @@
+module Jtl
+  module Converter
+    VERSION = "0.0.1"
+  end
+end
